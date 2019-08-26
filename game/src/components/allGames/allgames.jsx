61a2670b1288e406d07a8a2e2fb72c6447d92633
Annia Gomez <anniajesicagomezblanco@gmail.com>
2019-08-26 09:15:13 +0100

    added carousel

--- game/src/components/allGames/allgames.jsx
+++ game/src/components/allGames/allgames.jsx
@@ -2,8 +2,134 @@ import React, { Component } from 'react';
 import './allgame.css';
 
 class AllGame extends Component {
-    state = {}
+    state = {
+        featuredGame: [
+            { name: "game1" }, { name: "game2" }, { name: "game3" }, { name: "game4" },
+            { name: "game1" }, { name: "game2" }, { name: "game3" }, { name: "game4" }
+        ],
+        slotsGame: [
+            { name: "game1" }, { name: "game2" }, { name: "game3" }, { name: "game4" },
+            { name: "game1" }, { name: "game2" }, { name: "game3" }, { name: "game4" }
+        ],
+        tableGame: [
+            { name: "game1" }, { name: "game2" }, { name: "game3" }, { name: "game4" },
+            { name: "game1" }, { name: "game2" }, { name: "game3" }, { name: "game4" }
+        ],
+        listImgFeatured: [{ name: "game1" }, { name: "game2" }, { name: "game3" }, { name: "game4" }],
+        listImgSlots: [{ name: "game1" }, { name: "game2" }, { name: "game3" }, { name: "game4" }],
+        listImgTable: [{ name: "game1" }, { name: "game2" }, { name: "game3" }, { name: "game4" }],
+        posFeaturedPrevious: 0,
+        posSlotPrevious: 0,
+        posTablePrevious: 0,
+        posFeaturedNext: 3,
+        posSlotNext: 3,
+        posTableNext: 3
+    }
+
+    updatePosAdd(next, previous) {
+        next = next + 1;
+        previous = previous + 1;
+        return { next, previous };
+    }
+
+    updateListImg(list, img) {
+        list.splice(0, 1);
+        list.push(img);
+        return list;
+    }
+
+    nextImgGame(listTotal, list, posNext, posPrevious) {
+        if (posNext < (listTotal.length) - 1) {
+            let nextImg = listTotal[posNext + 1];
+            this.updateListImg(list, nextImg);
+            return this.updatePosAdd(posNext, posPrevious);
+        }
+        return { next: posNext, previous: posPrevious };
+    }
+
+    updateFeatured(posFeaturedNext, posFeaturedPrevious, listImgFeatured) {
+        this.setState({ listImgFeatured, posFeaturedNext, posFeaturedPrevious });
+    }
+
+    updateSlot(posSlotNext, posSlotPrevious, listImgSlots) {
+        this.setState({ posSlotNext, posSlotPrevious, listImgSlots });
+    }
+
+    updateTable(posTableNext, posTablePrevious, listImgTable) {
+        this.setState({ posTableNext, posTablePrevious, listImgTable });
+    }
+
+    handleFeaturedNext(listTotal, list) {
+        const { posFeaturedPrevious, posFeaturedNext } = this.state;
+        let { next, previous } = this.nextImgGame(listTotal, list, posFeaturedNext, posFeaturedPrevious);
+        this.updateFeatured(next, previous, list);
+    }
+
+    handleSlotNext(listTotal, list) {
+        const { posSlotPrevious, posSlotNext } = this.state;
+        let { next, previous } = this.nextImgGame(listTotal, list, posSlotNext, posSlotPrevious);
+        this.updateSlot(next, previous, list);
+    }
+
+    handleTableNext(listTotal, list) {
+        const { posTablePrevious, posTableNext } = this.state;
+        let { next, previous } = this.nextImgGame(listTotal, list, posTableNext, posTablePrevious);
+        this.updateTable(next, previous, list);
+    }
+
+    updatePosLess(next, previous) {
+        next = next - 1;
+        previous = previous - 1;
+        return { next, previous };
+    }
+
+    updatePrevious(list, previousImg) {
+        list.splice(3, 1);
+        list = [previousImg, ...list];
+        return list;
+    }
+
+    previousImgGame(listTotal, list, posNext, posPrevious) {
+        if (posPrevious > 0) {
+            let previousImg = listTotal[posPrevious - 1];
+            let newList = this.updatePrevious(list, previousImg);
+            let { next, previous } = this.updatePosLess(posNext, posPrevious);
+            return { next, previous, newList }
+        }
+        return { next: posNext, previous: posPrevious, newList: list };
+    }
+
+    handleFeaturedPrevious(listTotal, list) {
+        const { posFeaturedPrevious, posFeaturedNext } = this.state;
+        let { next, previous, newList } = this.previousImgGame(listTotal, list, posFeaturedNext, posFeaturedPrevious);
+        this.updateFeatured(next, previous, newList);
+    }
+
+    handleSlotPrevious(listTotal, list) {
+        const { posSlotPrevious, posSlotNext } = this.state;
+        let { next, previous, newList } = this.previousImgGame(listTotal, list, posSlotNext, posSlotPrevious);
+        this.updateSlot(next, previous, newList);
+    }
+
+    handleTablePrevious(listTotal, list) {
+        const { posTablePrevious, posTableNext } = this.state;
+        let { next, previous, newList } = this.previousImgGame(listTotal, list, posTableNext, posTablePrevious);
+        this.updateTable(next, previous, newList);
+    }
+
+    renderImg(tag) {
+        return <img src={`/assets/${tag.name}.jpg`} alt="game" />;
+    }
+
     render() {
+        const {
+            listImgFeatured,
+            listImgSlots,
+            listImgTable,
+            featuredGame,
+            slotsGame,
+            tableGame
+        } = this.state;
         return (
             <div className="border-m-top-50 background text-color block">
                 <div className="container block">
@@ -24,26 +150,44 @@ class AllGame extends Component {
                     <div className="text-color margin-left">
                         <div><span>FEATURED GAMES [14]</span></div>
                         <div className="imgGame">
-                            <img src="/assets/game1.jpg" alt="game" />
-                            <img className="imgGame" src="/assets/game2.jpg" alt="game" />
-                            <img className="imgGame" src="/assets/game3.jpg" alt="game" />
-                            <img className="imgGame" src="/assets/game4.jpg" alt="game" />
+                            <button className="btn-icon margin-left-15" onClick={() => this.handleFeaturedPrevious(featuredGame, listImgFeatured)}><img src="/assets/chev_left.png" alt="previous" /></button>
+                            {
+                                listImgFeatured.map((tag) => this.renderImg(tag))
+                            }
+                            <button className="btn-icon margin-right-15"
+                                onClick={() => this.handleFeaturedNext(featuredGame, listImgFeatured)}><img className="margin-left-90" src="/assets/chev_right.png" alt="next" /></button>
                         </div>
-
                     </div>
                     <div className="imgGame margin-left">
                         <div><span>SLOTS [55]</span></div>
-                        <img src="/assets/game1.jpg" alt="game" />
-                        <img src="/assets/game2.jpg" alt="game" />
-                        <img src="/assets/game3.jpg" alt="game" />
-                        <img src="/assets/game4.jpg" alt="game" />
+                        <button
+                            className="btn-icon margin-left-15"
+                            onClick={() => this.handleSlotPrevious(slotsGame, listImgSlots)}>
+                            <img src="/assets/chev_left.png" alt="previous" />
+                        </button>
+                        {
+                            listImgSlots.map((tag) => this.renderImg(tag))
+                        }
+                        <button
+                            className="btn-icon margin-right-15"
+                            onClick={() => this.handleSlotNext(slotsGame, listImgSlots)}
+                        ><img className="margin-left-90" src="/assets/chev_right.png" alt="next" /></button>
                     </div>
                     <div className="imgGame margin-left">
                         <div><span>TABLE GAMES [47]</span></div>
-                        <img src="/assets/game1.jpg" alt="game" />
-                        <img src="/assets/game2.jpg" alt="game" />
-                        <img src="/assets/game3.jpg" alt="game" />
-                        <img src="/assets/game4.jpg" alt="game" />
+                        <button
+                            className="btn-icon margin-left-15"
+                            onClick={() => this.handleTablePrevious(tableGame, listImgTable)}>
+                            <img src="/assets/chev_left.png" alt="previous" />
+                        </button>
+                        {
+                            listImgTable.map(tag => this.renderImg(tag))
+                        }
+                        <button
+                            className="btn-icon margin-right-15"
+                            onClick={() => this.handleTableNext(tableGame, listImgTable)}>
+                            <img className="margin-left-90" src="/assets/chev_right.png" alt="next" />
+                        </button>
                     </div>
                 </div>
             </div>
